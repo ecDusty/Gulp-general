9f64efcee08be4926306533163973b0bf63e19ef
Erin <help@unlikelyit.com>
2017-11-14 22:10:20 +0800

    Getting started, Laying out Vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+//Where is your project located???
+var Projects = {
+    Conrad: '../../Users/mulli/OneDrive/GitHub/lianatech-CMS/Conrad-landing/'
+}
+
 /*=============================
 
         DEPENDENCIES
@@ -7,11 +12,48 @@
 var gulp = require('gulp');
 
 
+/*========================
+       VARIABLES
+=========================*/
+
 
 //Text Variables
 var strt = '>---Starting ',
-    end = ' Task---<';
+    cls = ' Task---<';
 
 //Directory Variables
 var $test = 'dev/',
-    $liv = 'live/'
+    $liv = 'live/',
+    $src = 'src/',
+    $active = '';
+
+/*====================
+    Custom Functions
+=====================*/
+
+function talk (a,t) {
+    if (t) 
+        console.log(strt+a+cls);
+    else   
+        console.log(a);
+}
+
+
+/*========================
+       TASKS
+=========================*/
+
+//START
+gulp.task('start', function () {
+    $active = Projects.Conrad;
+    //add task to delete dev and live files
+});
+
+//SET THE PARAMATERS
+gulp.task('set-live', function () {
+    $active = $liv;
+    console.log('Now the active directory: '+$active);
+});
+       
+gulp.task('default',['start','set-live']);
+          
